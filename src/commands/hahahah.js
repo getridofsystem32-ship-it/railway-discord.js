925bd69751dcda3e5c494b2693efda3de8cf52da
getridofsystem32-ship-it <getridofsystem32@gmail.com>
2026-07-17 18:25:06 -0400

    Update hahahah.js

--- src/commands/hahahah.js
+++ src/commands/hahahah.js
@@ -1,1 +1,11 @@
-j
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("haha")
+    .setDescription("evil ai laughs"),
+
+  async execute(interaction) {
+    await interaction.reply("im fricking evil claude ahahhahah");
+  }
+};
